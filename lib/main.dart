761e5f40897d5cbf85edfe34689a57359d714245
LoneCobra <lonecobragaming@gmail.com>
2022-06-19 15:26:40 +0530

    Day 3 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shoppingapp/home_page.dart';
+import 'package:shoppingapp/pages/home_page.dart';
+import 'package:shoppingapp/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.grey),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
