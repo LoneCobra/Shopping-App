ba0ed7d2741447cea65f85be1d26735eb9d50089
LoneCobra <lonecobragaming@gmail.com>
2022-06-19 17:31:54 +0530

    Day 4 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shoppingapp/pages/home_page.dart';
 import 'package:shoppingapp/pages/login_page.dart';
 
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.grey),
+      theme: ThemeData(
+          primarySwatch: Colors.grey,
+          fontFamily: GoogleFonts.montserrat().fontFamily),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
